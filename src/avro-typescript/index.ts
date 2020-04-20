ea437be5e60c486ca289cefe9f35b3bdf6413621
Sondre Mære Overskaug <sondremo@gmail.com>
2020-04-20 15:20:58 +0200

    Revert "fix: make sure both enums and interfaces are prefixed with the root record type to prevent naming collisions when having multiple avro schemas"
    This reverts commit 66e1c1be688f7d8853d6c016c6a92c24179f46ee.

--- src/avro-typescript/index.ts
+++ src/avro-typescript/index.ts
@@ -49,19 +49,16 @@ export const avroToTypeScript = (recordType: RecordType | EnumType): string => {
 const stripNamespace = (name: string) => name.split('.').pop() as string;
 
 /** Convert an Avro Record type. Return the name, but add the definition to the file */
-const convertRecord = (recordType: RecordType, buffer: string[], rootRecordType?: RecordType): string => {
+const convertRecord = (recordType: RecordType, buffer: string[], parentRecordType?: RecordType): string => {
   const doc = document(recordType);
   let cleanName = '';
-  if (rootRecordType) {
-    cleanName = `I${stripNamespace(rootRecordType.name)}${stripNamespace(recordType.name)}`;
+  if (parentRecordType) {
+    cleanName = `I${stripNamespace(parentRecordType.name)}${stripNamespace(recordType.name)}`;
   } else {
     cleanName = `I${stripNamespace(recordType.name)}`;
   }
-  if (!rootRecordType) {
-    rootRecordType = recordType;
-  }
   const interfaceDef = `${doc}export interface ${cleanName} {
-${recordType.fields.map(field => convertFieldDec(field, buffer, rootRecordType)).join('\n')}
+${recordType.fields.map(field => convertFieldDec(field, buffer, recordType)).join('\n')}
 }
 `;
   buffer.push(interfaceDef);
@@ -70,17 +67,16 @@ ${recordType.fields.map(field => convertFieldDec(field, buffer, rootRecordType))
 };
 
 /** Convert an Avro Enum type. Return the name, but add the definition to the file */
-const convertEnum = (enumType: EnumType, buffer: string[], rootRecordType?: RecordType): string => {
+const convertEnum = (enumType: EnumType, buffer: string[]): string => {
   const doc = document(enumType);
-  const enumName: string = rootRecordType ? `${rootRecordType.name}${enumType.name}` : enumType.name;
-  const enumDef = `${doc}export enum ${enumName} {
+  const enumDef = `${doc}export enum ${enumType.name} {
 \t${enumType.symbols.map(s => `${s} = '${s}'`).join(',\n\t')}
 }\n`;
   buffer.push(enumDef);
-  return enumName;
+  return enumType.name;
 };
 
-const convertType = (type: Type, buffer: string[], rootRecordType?: RecordType): string => {
+const convertType = (type: Type, buffer: string[], parentRecordType?: RecordType): string => {
   // if it's just a name, then use that
   if (typeof type === 'string') {
     return convertPrimitive(type);
@@ -88,35 +84,35 @@ const convertType = (type: Type, buffer: string[], rootRecordType?: RecordType):
     const isUnique = (value: any, index: number, arr: any[]) => arr.indexOf(value) === index;
     // array means a Union. Use the names and call recursively
     return type
-      .map(t => stripNamespace(convertType(t, buffer, rootRecordType)))
+      .map(t => stripNamespace(convertType(t, buffer)))
       .map(n => (interfaces.hasOwnProperty(n) ? interfaces[n] : n))
       .filter(isUnique)
       .join(' | ');
   } else if (isRecordType(type)) {
     // record, use the name and add to the buffer
-    return convertRecord(type, buffer, rootRecordType);
+    return convertRecord(type, buffer, parentRecordType);
   } else if (isArrayType(type)) {
     const isUnion = (s: string) => s.indexOf('|') >= 0;
     // array, call recursively for the array element type
-    const name = stripNamespace(convertType(type.items, buffer, rootRecordType));
+    const name = stripNamespace(convertType(type.items, buffer));
     const properName = interfaces.hasOwnProperty(name) ? interfaces[name] : name;
     return isUnion(properName) ? `Array<${properName}>` : `${properName}[]`;
     // return `${convertType(type.items, buffer)}[]`;
   } else if (isMapType(type)) {
     // Dictionary of types, string as key
-    return `{ [key: string]: ${convertType(type.values, buffer, rootRecordType)} }`;
+    return `{ [key: string]: ${convertType(type.values, buffer)} }`;
   } else if (isEnumType(type)) {
     // array, call recursively for the array element type
-    return convertEnum(type, buffer, rootRecordType);
+    return convertEnum(type, buffer);
   } else {
     console.error('Cannot work out type', type);
     return 'UNKNOWN';
   }
 };
 
-const convertFieldDec = (field: Field, buffer: string[], rootRecordType?: RecordType): string => {
+const convertFieldDec = (field: Field, buffer: string[], parentRecordType?: RecordType): string => {
   const doc = document(field, '\t');
-  const type = convertType(field.type, buffer, rootRecordType);
+  const type = convertType(field.type, buffer, parentRecordType);
   const replacedType = interfaces.hasOwnProperty(type) ? interfaces[type] : type;
   return `${doc}\t${field.name}${isOptional(field.type) ? '?' : ''}: ${replacedType};`;
 };
